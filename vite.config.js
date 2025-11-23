030b2887b0e873b6d09dd38ab54c1459fff2dad4
abdelaziz-sekouti <abdelaziz-sekouti@hotmail.com>
2025-11-23 14:23:00 +0100

    path for main js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/my_react_vite_app/',
-  
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor'
+            if (id.includes('lodash')) return 'lodash'
+            return 'vendor'
+          }
+        }
+      }
+    },
+    // increase limit (kb) or set to Infinity to disable warning
+    chunkSizeWarningLimit: 600
+  }
 })
